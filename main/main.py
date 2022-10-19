34735e1a6f45df5ee330bfa18f0b16891b68cf86
Oliver Frisvoll <ocfris97@gmail.com>
2022-10-20 01:10:17 +0200

    initial commit

---
+++ main/main.py
@@ -0,0 +1,12 @@
+class Asset:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.name} {self.price} {self.quantity}"
+
+
+class Currency:
+    pass
